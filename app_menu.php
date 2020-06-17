8774331b8bdbd62ff53ee2d8062d36e4a4fc7ded
hershyheilpern <hershyheilpern@gmail.com>
2020-06-17 13:22:48 -0400

    Update app_menu.php

--- app_menu.php
+++ app_menu.php
@@ -1,6 +1,6 @@
 <?php
 
-$apps[$x]['menu'][0]['title']['en-us'] = 'Messages';
+$apps[$x]['menu'][0]['title']['en-us'] = 'Message-Plus';
 $apps[$x]['menu'][0]['title']['es-cl'] = '';
 $apps[$x]['menu'][0]['title']['fr-fr'] = '';
 $apps[$x]['menu'][0]['title']['fr-ca'] = '';
@@ -9,10 +9,10 @@ $apps[$x]['menu'][0]['title']['pl'] = '';
 $apps[$x]['menu'][0]['title']['sv-se'] = '';
 $apps[$x]['menu'][0]['title']['uk'] = '';
 $apps[$x]['menu'][0]['title']['de-at'] = '';
-$apps[$x]['menu'][0]['uuid'] = 'e4a805b7-8881-41a8-b319-981d67f5e13d';
+$apps[$x]['menu'][0]['uuid'] = 'b3a0f17e-7c4f-4ce4-a535-463ed646b00d';
 $apps[$x]['menu'][0]['parent_uuid'] = 'fd29e39c-c936-f5fc-8e2b-611681b266b5';
 $apps[$x]['menu'][0]['category'] = 'internal';
-$apps[$x]['menu'][0]['path'] = '/app/messages/messages.php';
+$apps[$x]['menu'][0]['path'] = '/app/message-plus/messages.php';
 $apps[$x]['menu'][0]['groups'][] = 'superadmin';
 //$apps[$x]['menu'][0]['groups'][] = 'admin';
 //$apps[$x]['menu'][0]['groups'][] = 'user';
